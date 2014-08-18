465803c5baefc9a4eab2929c64a809753a717d71
aynik <pablovidal85@gmail.com>
2014-08-18 18:05:34 +0200

    reorganized

--- test.js
+++ test.js
@@ -13,63 +13,63 @@ var testOut = function(t, success){
 test("Should run all tests", function(t){ 
     t.test("sx --completion", function(t){
         t.plan(3);
-        exec('./sx --completion', testOut(t, function(stdout){
-            t.same(fs.readFileSync('./completion.sh', 'utf-8'), stdout);
+        exec('./bin/sx --completion', testOut(t, function(stdout){
+            t.same(fs.readFileSync('./lib/completion.sh', 'utf-8'), stdout);
         }));
     });
 
     t.test("sx --pretty", function(t){
         t.plan(3);
-        exec('./sx -p "({a:1})"', testOut(t, function(stdout){
+        exec('./bin/sx -p "({a:1})"', testOut(t, function(stdout){
             t.same("{\n    \"a\": 1\n}", stdout);
         }));
     });
 
     t.test("sx --json", function(t){
         t.plan(3);
-        exec('./sx "({a:\'b\'})" | ./sx -ji i.a', testOut(t, function(stdout){
+        exec('./bin/sx "({a:\'b\'})" | ./bin/sx -ji i.a', testOut(t, function(stdout){
             t.same("b", stdout);
         }));
     });
 
     t.test("sx --input", function(t){
         t.plan(3);
-        exec('./sx 3 | ./sx -i +i+5', testOut(t, function(stdout){
+        exec('./bin/sx 3 | ./bin/sx -i +i+5', testOut(t, function(stdout){
             t.same("8", stdout);
         }));
     });
 
     t.test("sx --list", function(t){
         t.plan(3);
-        exec('for n in 1 2 3; do echo $n; done | ./sx -l "l[1]"', testOut(t, function(stdout){
+        exec('for n in 1 2 3; do echo $n; done | ./bin/sx -l "l[1]"', testOut(t, function(stdout){
             t.same("2", stdout);
         }));
     });
 
     t.test("sx --reduce", function(t){
         t.plan(3);
-        exec('for n in 1 2 3; do echo $n; done | ./sx -lir 0 +i+r', testOut(t, function(stdout){
+        exec('for n in 1 2 3; do echo $n; done | ./bin/sx -lir 0 +i+r', testOut(t, function(stdout){
             t.same("6", stdout);
         }));
     });
 
     t.test("sx --async", function(t){
         t.plan(3);
-        exec('./sx -a "setTimeout(a.bind(0, 1), 100)"', testOut(t, function(stdout){
+        exec('./bin/sx -a "setTimeout(a.bind(0, 1), 100)"', testOut(t, function(stdout){
             t.same("1", stdout);
         }));
     });
 
     t.test("sx --filter", function(t){
         t.plan(3);
-        exec('./sx 1 | ./sx -fi i==1', testOut(t, function(stdout){
+        exec('./bin/sx 1 | ./bin/sx -fi i==1', testOut(t, function(stdout){
             t.same("1", stdout);
         }));
     });
 
     t.test("sx --string", function(t){
         t.plan(3);
-        exec('./sx -s "Function()"', testOut(t, function(stdout){
+        exec('./bin/sx -s "Function()"', testOut(t, function(stdout){
             t.same("// vim: filetype=javascript\nfunction anonymous() {\n\n}", stdout);
         }));
     });
